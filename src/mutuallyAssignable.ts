bc0c2f4f41b39d81235192af444b22f6d785fdf6
Davis Sorenson <davis.sorenson@gmail.com>
2021-05-25 23:07:49 +0300

    "clean up" (?) after testing assignability

--- src/mutuallyAssignable.ts
+++ src/mutuallyAssignable.ts
@@ -2,6 +2,8 @@ import { createProjectSync, ts } from "@ts-morph/bootstrap";
 import { Type } from "ts-morph";
 import { typeChecker } from "./project";
 
+const TYPE_CHECK_FILENAME = "assignmentCheck.ts";
+
 const inMemoryProject = createProjectSync({ useInMemoryFileSystem: true });
 
 const makeTypeTest = (a: Type, b: Type): string =>
@@ -15,9 +17,11 @@ const p: A = b;
 const q: B = a;`;
 
 const mutuallyAssignable = (a: Type, b: Type): boolean => {
-  inMemoryProject.createSourceFile("assignmentCheck.ts", makeTypeTest(a, b));
+  inMemoryProject.createSourceFile(TYPE_CHECK_FILENAME, makeTypeTest(a, b));
   const program = inMemoryProject.createProgram();
   const diagnostics = ts.getPreEmitDiagnostics(program);
+
+  inMemoryProject.removeSourceFile(TYPE_CHECK_FILENAME);
 
   // good enough for now
   return diagnostics.length === 0;

fbfb7d483963e2676b2210337db65a9ab8a71502
Davis Sorenson <davis.sorenson@gmail.com>
2021-05-28 23:55:51 +0300

    Remove old function index building code

--- src/buildFnIndex.ts
+++ src/buildFnIndex.ts
@@ -1,18 +1,7 @@
-import fnHashFromFn from "./fnHashFromFn";
-import { Fn, FnHash, FnIndex } from "./types";
+import { Fn, FnIndex } from "./types";
 
-const buildFnIndex = (fns: Fn[]): FnIndex =>
-  fns.reduce((allFnHashes, fn) => {
-    const hash = fnHashFromFn(fn);
-    const fnsForHash = allFnHashes.get(hash);
-
-    if (fnsForHash) {
-      allFnHashes.set(hash, [...fnsForHash, fn]);
-    } else {
-      allFnHashes.set(hash, [fn]);
-    }
-
-    return allFnHashes;
-  }, new Map<FnHash, Fn[]>());
+const buildFnIndex = (_fns: Fn[]): FnIndex => {
+  throw new Error("Not implemented");
+};
 
 export default buildFnIndex;

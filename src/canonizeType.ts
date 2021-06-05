dd58694aaf480320ae7c2aa1a6a6744d05fc76e6
Davis Sorenson <davis.sorenson@gmail.com>
2021-06-05 14:44:51 +0300

    Render tuples within function types

--- src/canonizeType.ts
+++ src/canonizeType.ts
@@ -2,17 +2,16 @@ import R from "ramda";
 import {
   ArrowFunction,
   FunctionTypeNode,
+  Identifier,
   KindToNodeMappings,
-  NamedTupleMember,
   Node,
   PropertySignature,
   SyntaxKind,
+  SyntaxList,
+  ts,
   TupleTypeNode,
-  TypeAliasDeclaration,
-  TypeFormatFlags,
   TypeLiteralNode,
 } from "ts-morph";
-import { typeChecker } from "./project";
 import { getChildOfKind } from "./utils/getChildOfKind";
 
 const PARAMETER_NAMES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -28,81 +27,51 @@ const getNthParameterName = (n: number, depth: number): string => {
 
 const removeWhiteSpace = (s: string): string => s.replaceAll(/\s/g, "");
 
-const renderWithoutWhitespace = (n: Node): string =>
+const renderWithoutWhitespace = (ns: Node[], depth: number): string =>
+  removeWhiteSpace(ns.map((it) => canonizeType(it, depth)).join(""));
+
+const renderChildrenWithoutWhitespace = (n: Node, depth: number): string =>
   removeWhiteSpace(
     n
       .getChildren()
-      .map((it) => it.getText())
+      .map((it) => canonizeType(it, depth))
       .join("")
-  );
-
-const getTypeText = (node: Node): string =>
-  typeChecker.getTypeText(
-    node.getType(),
-    undefined,
-    TypeFormatFlags.UseAliasDefinedOutsideCurrentScope |
-      TypeFormatFlags.NoTruncation |
-      TypeFormatFlags.UseFullyQualifiedType
   );
 
 type FnType = FunctionTypeNode | ArrowFunction;
 
-const canonizeFnType = (fnType: FnType): string => {
-  const inner = (scope: FnType, depth = 0): void => {
-    const parameters = scope.getParameters();
-    parameters.forEach((param, i): void => {
-      param.rename(getNthParameterName(i, depth));
+const canonizeFnType = (fnType: FnType, depth: number): string => {
+  const parameters = fnType.getParameters();
 
-      const fnChild = getChildOfKind(param, SyntaxKind.FunctionType);
-
-      if (fnChild) {
-        inner(fnChild, depth + 1);
-      }
-    });
-  };
-
-  inner(fnType);
-
-  const kind = fnType.getKind();
-
-  if (kind === SyntaxKind.ArrowFunction) {
-    return removeWhiteSpace(getTypeText(fnType));
-  }
-
-  return renderWithoutWhitespace(fnType);
-};
-
-const canonizeTupleType = (tupleType: TupleTypeNode): string => {
-  const namedTupleStack: NamedTupleMember[] = [];
-
-  const inner = (scope: TupleTypeNode): void => {
-    const members = scope.getElements();
-    members.forEach((member) => {
-      const kind = member.getKind();
-      if (kind === SyntaxKind.NamedTupleMember) {
-        namedTupleStack.push(member.asKindOrThrow(kind));
-      }
-
-      const nestedTuple = getChildOfKind(member, SyntaxKind.TupleType);
-
-      if (nestedTuple) {
-        inner(nestedTuple);
-      }
-    });
-  };
-
-  inner(tupleType);
-
-  namedTupleStack.reverse().forEach((namedTuple) => {
-    const name = namedTuple.getName();
-    const existingText = removeWhiteSpace(namedTuple.getText());
-    namedTuple.replaceWithText(existingText.replace(`${name}:`, ""));
+  parameters.forEach((param, i): void => {
+    param.rename(getNthParameterName(i, depth));
   });
 
-  return renderWithoutWhitespace(tupleType);
+  return renderChildrenWithoutWhitespace(fnType, depth);
 };
 
-const canonizeTypeLiteral = (typeLiteral: TypeLiteralNode): string => {
+const canonizeTupleType = (tupleType: TupleTypeNode, depth: number): string => {
+  const members = tupleType.getElements();
+
+  members.map((it) =>
+    it.transform((traversal) => {
+      const node = traversal.visitChildren();
+
+      if (ts.isNamedTupleMember(node)) {
+        return node.type;
+      }
+
+      return node;
+    })
+  );
+
+  return renderChildrenWithoutWhitespace(tupleType, depth);
+};
+
+const canonizeTypeLiteral = (
+  typeLiteral: TypeLiteralNode,
+  depth: number
+): string => {
   const typeLiteralStack: R.KeyValuePair<
     PropertySignature,
     PropertySignature
@@ -138,50 +107,103 @@ const canonizeTypeLiteral = (typeLiteral: TypeLiteralNode): string => {
     })
   );
 
-  return renderWithoutWhitespace(typeLiteral);
+  return renderChildrenWithoutWhitespace(typeLiteral, depth);
 };
 
-const canonizeType = (
-  type: FunctionTypeNode | ArrowFunction | TypeAliasDeclaration
-): string => {
+const canonizeSyntaxList = (syntaxList: SyntaxList, depth: number): string => {
+  const typesExcludedFromRendering = [SyntaxKind.ExportKeyword];
+  const filteredChildren = syntaxList
+    .getChildren()
+    .filter((it) => !typesExcludedFromRendering.includes(it.getKind()));
+
+  return renderWithoutWhitespace(filteredChildren, depth);
+};
+
+const renderIdentifier = (identifier: Identifier): string => {
+  const typesIncludedInRendering = [
+    SyntaxKind.Parameter,
+    SyntaxKind.FunctionType,
+  ];
+  const parentsTypes = identifier.getAncestors().map((it) => it.getKind());
+
+  if (R.intersection(typesIncludedInRendering, parentsTypes).length > 0) {
+    return identifier.getText();
+  }
+
+  return "";
+};
+
+const canonizeType = (type: Node, depth: number): string => {
   const kind = type.getKind();
 
   switch (kind) {
     case SyntaxKind.FunctionType:
       return canonizeFnType(
-        type as KindToNodeMappings[SyntaxKind.FunctionType]
+        type as KindToNodeMappings[SyntaxKind.FunctionType],
+        depth
       );
+
     case SyntaxKind.ArrowFunction:
       return canonizeFnType(
-        type as KindToNodeMappings[SyntaxKind.ArrowFunction]
+        type as KindToNodeMappings[SyntaxKind.ArrowFunction],
+        depth
       );
-    // TODO: replace this flat logic with something recursive
+
+    case SyntaxKind.TupleType:
+      return canonizeTupleType(
+        type as KindToNodeMappings[SyntaxKind.TupleType],
+        depth
+      );
+
+    case SyntaxKind.TypeLiteral:
+      return canonizeTypeLiteral(
+        type as KindToNodeMappings[SyntaxKind.TypeLiteral],
+        depth
+      );
+
     case SyntaxKind.TypeAliasDeclaration:
-      const fnType = getChildOfKind(type, SyntaxKind.FunctionType);
-      const tupleType = getChildOfKind(type, SyntaxKind.TupleType);
-      const typeLiteral = getChildOfKind(type, SyntaxKind.TypeLiteral);
-
-      if (fnType) {
-        return canonizeFnType(fnType);
-      }
-
-      if (tupleType) {
-        return canonizeTupleType(tupleType);
-      }
-
-      if (typeLiteral) {
-        return canonizeTypeLiteral(typeLiteral);
-      }
-
-      throw new Error(
-        `Unable to handle node of kind ${type.getKindName()}. Children: ${type
-          .getChildren()
-          .map((it) => it.getKindName())}`
+      return R.join(
+        "",
+        type.getChildren().map((it) => canonizeType(it, depth + 0))
       );
+
+    case SyntaxKind.SyntaxList:
+      return canonizeSyntaxList(
+        type as KindToNodeMappings[SyntaxKind.SyntaxList],
+        depth
+      );
+
+    case SyntaxKind.Parameter:
+      return renderChildrenWithoutWhitespace(type, depth);
+
+    case SyntaxKind.Identifier:
+      return renderIdentifier(
+        type as KindToNodeMappings[SyntaxKind.Identifier]
+      );
+
+    case SyntaxKind.SemicolonToken:
+    case SyntaxKind.TypeKeyword:
+    case SyntaxKind.EqualsToken:
+      return "";
+
+    case SyntaxKind.OpenParenToken:
+    case SyntaxKind.CloseParenToken:
+    case SyntaxKind.EqualsGreaterThanToken:
+    case SyntaxKind.OpenBracketToken:
+    case SyntaxKind.CloseBracketToken:
+    case SyntaxKind.CommaToken:
+    case SyntaxKind.ArrayType:
+    case SyntaxKind.ColonToken:
+    case SyntaxKind.StringKeyword:
+    case SyntaxKind.NumberKeyword:
+      return type.getText();
 
     default:
-      return removeWhiteSpace(getTypeText(type));
+      throw new Error(`Unable to handle node of kind ${type.getKindName()}.`);
+    // return removeWhiteSpace(getTypeText(type));
   }
 };
 
-export default canonizeType;
+const exportedCanonizeType = (type: Node): string => canonizeType(type, 0);
+
+export default exportedCanonizeType;

618258479f514cb64bc1adaca83eb62ea9211c2f
Davis Sorenson <davis.sorenson@gmail.com>
2021-06-09 23:12:07 +0300

    Use in memory file system for project

--- src/project.ts
+++ src/project.ts
@@ -1,6 +1,6 @@
 import { Project } from "ts-morph";
 
-const project = new Project();
+const project = new Project({ useInMemoryFileSystem: true });
 export const typeChecker = project.getTypeChecker();
 
 export default project;

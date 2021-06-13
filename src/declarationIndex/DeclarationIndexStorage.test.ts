577f82e1e9b9cfc49072633094da8d6f9533ed96
Davis Sorenson <davis.sorenson@gmail.com>
2021-06-13 21:02:44 +0300

    Add test for DeclarationIndexStorage#removeDeclarationsForFilePath

--- src/declarationIndex/DeclarationIndexStorage.test.ts
+++ src/declarationIndex/DeclarationIndexStorage.test.ts
@@ -4,27 +4,29 @@ import {
 } from "./DeclarationIndex";
 import DeclarationIndexStorage from "./DeclarationIndexStorage";
 
+const filePaths = ["file-path-1", "file-path-2"];
+
 const declarationsWithMetadata: DeclarationWithMetaData[] = [
   {
     name: "declaration-1",
     declaration: {} as any,
     originalTypeString: "original-type-string-1",
     lineNo: 42,
-    filePath: "file-path-1",
+    filePath: filePaths[0],
   },
   {
     name: "declaration-2",
     declaration: {} as any,
     originalTypeString: "original-type-string-2",
     lineNo: 1337,
-    filePath: "file-path-1",
+    filePath: filePaths[0],
   },
   {
     name: "declaration-3",
     declaration: {} as any,
     originalTypeString: "original-type-string-3",
     lineNo: 123,
-    filePath: "file-path-2",
+    filePath: filePaths[1],
   },
 ];
 
@@ -46,24 +48,71 @@ const hashesAndDeclarations: HashAndDeclarationWithMetadata[] = [
 describe("DeclarationIndexStorage", () => {
   let storage: DeclarationIndexStorage;
 
+  const setupTest = () => {
+    storage = new DeclarationIndexStorage();
+    storage.addDeclarations(hashesAndDeclarations);
+  };
+  const getHashToMetaData = () =>
+    Array.from(storage.getHashToMetaData().entries());
+  const getFilePathToHashes = () =>
+    Array.from(storage.getFilePathToHashes().entries());
+
   describe("addDeclarations", () => {
     beforeEach(() => {
-      storage = new DeclarationIndexStorage();
-      storage.addDeclarations(hashesAndDeclarations);
+      setupTest();
     });
 
     it("stores declarations in hash to metadata map", () => {
-      expect(Array.from(storage.getHashToMetaData().entries())).toEqual([
+      expect(getHashToMetaData()).toEqual([
         ["hash-1", [declarationsWithMetadata[0], declarationsWithMetadata[2]]],
         ["hash-2", [declarationsWithMetadata[1]]],
       ]);
     });
 
     it("stores hashes in file path to hashes map", () => {
-      expect(Array.from(storage.getFilePathToHashes().entries())).toEqual([
+      expect(getFilePathToHashes()).toEqual([
         ["file-path-1", ["hash-1", "hash-2"]],
         ["file-path-2", ["hash-1"]],
       ]);
+    });
+  });
+
+  describe("removeDeclarationsForFilePath", () => {
+    describe(filePaths[0], () => {
+      beforeEach(() => {
+        setupTest();
+        storage.removeDeclarationsForFilePath(filePaths[0]);
+      });
+
+      it("removes declarations from hash to metadata map", () => {
+        expect(getHashToMetaData()).toEqual([
+          ["hash-1", [declarationsWithMetadata[2]]],
+        ]);
+      });
+
+      it("removes hashes from file path to hashes map", () => {
+        expect(getFilePathToHashes()).toEqual([["file-path-2", ["hash-1"]]]);
+      });
+    });
+
+    describe(filePaths[1], () => {
+      beforeEach(() => {
+        setupTest();
+        storage.removeDeclarationsForFilePath(filePaths[1]);
+      });
+
+      it("removes declarations from hash to metadata map", () => {
+        expect(getHashToMetaData()).toEqual([
+          ["hash-1", [declarationsWithMetadata[0]]],
+          ["hash-2", [declarationsWithMetadata[1]]],
+        ]);
+      });
+
+      it("removes hashes from file path to hashes map", () => {
+        expect(getFilePathToHashes()).toEqual([
+          ["file-path-1", ["hash-1", "hash-2"]],
+        ]);
+      });
     });
   });
 });
